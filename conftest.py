13e4799fade0d20e067f58674a11b8db922f3656
Kkzzzq <snorris068@gmail.com>
2026-03-23 10:25:04 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,9 +9,14 @@ from config import settings
 def pytest_sessionstart(session):
     env_dir = os.path.join(os.getcwd(), settings.ALLURE_RESULTS_DIR)
     env_path = os.path.join(env_dir, "environment.properties")
+
     os.makedirs(env_dir, exist_ok=True)
 
-    runner = "GitHub Actions" if os.getenv("GITHUB_ACTIONS") == "true" else "Local"
+    runner = (
+        "Docker Compose (CI)"
+        if os.getenv("GITHUB_ACTIONS") == "true"
+        else "Docker Compose (Local)"
+    )
 
     with open(env_path, "w", encoding="utf-8") as f:
         f.write(f"Python={platform.python_version()}\n")
